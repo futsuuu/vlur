23551c89f9aa57e3a1ffbc59037e65768a76b2f8
futsuuu <futsuuu123@gmail.com>
2023-12-17 20:43:12 +0900

    refactor(vlur): move into vlur directory

--- vlur/build.rs
+++ vlur/build.rs
@@ -4,11 +4,11 @@ fn main() -> anyhow::Result<()> {
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let out_dir = Path::new(&out_dir);
 
-    println!("cargo:rerun-if-changed=lua/vlur/nvim.lua");
-    println!("cargo:rerun-if-changed=scripts/dump_file.lua");
+    println!("cargo:rerun-if-changed=../lua/vlur/nvim.lua");
+    println!("cargo:rerun-if-changed=../scripts/dump_file.lua");
     Command::new("nvim")
-        .args(["-l", "scripts/dump_file.lua"])
-        .arg("lua/vlur/nvim.lua")
+        .args(["-l", "../scripts/dump_file.lua"])
+        .arg("../lua/vlur/nvim.lua")
         .arg(out_dir.join("nvim.luac"))
         .status()?;
 
